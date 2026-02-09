86a155b9d31565900678bbebeb276f39f091ba51
ninickut <nicoletrindade@hotmail.com>
2026-02-09 15:14:58 -0300

    Cria estado global do app: Pomodoro e To-Do List

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('App iniciado');
+
+   const state = {
+    pomodoro: {
+      modo: 'pomodoro', // 'pomodoro', 'descansoCurto', 'descansoLongo'
+      tempoRestante: 25 * 60, // em segundos
+      rodando: false,
+    },
+    tarefas: [], // cada tarefa: { id, texto, feito }
+  };
+
+  console.log(state);
+  
 });
